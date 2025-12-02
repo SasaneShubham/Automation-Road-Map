df72b21309183f28deeda114a132eaa744141576
SasaneShubham <shubhsasane07@gmail.com>
2025-12-02 08:59:53 +0530

    Adding cloned java class

---
+++ Java_Programs/src/Variables/Clone_Class.java
@@ -0,0 +1,45 @@
+package Variables;
+
+public class Clone_Class {
+
+	
+
+
+
+		public static void main(String[] args) {
+			
+			//declaration
+			
+			String Name;
+			int RollNo;
+			char Grade;
+		    byte marks;
+		    float percentage;
+		    
+		    
+		    //initialisation
+		    
+		    Name="Shubham Sasane";
+		    RollNo=23;
+		    Grade='A';
+		    marks=99;
+		    percentage=98.33f;
+		    
+		    //usage
+		    
+		    System.out.println("Name :"+ Name);
+		    System.out.println("RollNo :"+ RollNo);
+		    System.out.println("Grade  :" + Grade);
+		    System.out.println("marks  :" + marks);
+		    System.out.println("percentage  :"+ percentage );
+		
+			
+			
+			
+			
+		}
+		
+		
+	}
+
+
